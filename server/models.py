3fe0ee727a1ddbd02d0be6077539403ff1d73b17
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-07-18 21:36:12 +0300

    created exercise model with validation

--- server/models.py
+++ server/models.py
@@ -1,4 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from sqlalchemy import CheckConstraint
 
 db = SQLAlchemy()
+
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    equipment_needed = db.Column(db.Boolean, nullable=False)
+
+    
+    
+    @validates("name")
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Exercise name is required")
+
+        if len(name) < 3:
+            raise ValueError(
+                "Exercise name must be at least 3 characters"
+            )
+
+        return name
+    
+    def __repr__(self):
+        return f"<Exercise {self.name}>"
